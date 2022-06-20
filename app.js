f6e8902377f1777a9aa5d35c64f95544d1f0fa97
chasechri <christensonchase451@gmail.com>
2022-06-20 14:10:42 -0500

    creted html pg

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require("fs");
 
-const printProfileData = (profileDataArr) => {
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const generatePage = require("./src/page-template");
 
-  console.log("================");
+const profileDataArgs = process.argv.slice(2);
 
-  // IS the same as this ...
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
+
+  console.log("Portfolio complete! checkout index.html to see the output!");
+});
